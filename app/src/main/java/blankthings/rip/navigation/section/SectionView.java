540c557a6625228247c614d875ed27ae664772f2
Iosif Vilcea <iosifvilcea@gmail.com>
2017-04-10 23:17:07 -0500

    Added RemoveItem Decoration.

--- app/src/main/java/blankthings/rip/navigation/section/SectionView.java
+++ app/src/main/java/blankthings/rip/navigation/section/SectionView.java
@@ -2,7 +2,12 @@ package blankthings.rip.navigation.section;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -16,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import blankthings.rip.MainActivity;
 import blankthings.rip.R;
 import blankthings.rip.sections.album.OnItemClickListener;
 
@@ -48,7 +54,8 @@ public class SectionView extends FrameLayout {
         final ItemTouchHelper itemTouchHelper = new ItemTouchHelper(itemTouchCb);
         itemTouchHelper.attachToRecyclerView(recyclerView);
 
-        recyclerView.addItemDecoration(itemDecoration);
+        RemoveItemDecoration removeItemDecoration = new RemoveItemDecoration();
+        recyclerView.addItemDecoration(removeItemDecoration);
     }
 
 
@@ -70,39 +77,62 @@ public class SectionView extends FrameLayout {
 
 
     private ItemTouchHelper.SimpleCallback itemTouchCb =
-            new ItemTouchHelper.SimpleCallback(ItemTouchHelper.RIGHT, ItemTouchHelper.RIGHT) {
-        @Override
-        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
-                              RecyclerView.ViewHolder target) {
-            return false;
-        }
+            new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
 
-        @Override
-        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-            if (listener != null) {
-                final int position = viewHolder.getAdapterPosition();
-                listener.onItemSwiped(position);
-                adapter.removeItem(position);
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(getContext(), R.drawable.ic_add_white_24dp);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                xMarkMargin = (int) getContext().getResources().getDimension(R.dimen.iconSideMargin);
+                initiated = true;
             }
-        }
-    };
 
 
-    private RecyclerView.ItemDecoration itemDecoration = new RecyclerView.ItemDecoration() {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                  RecyclerView.ViewHolder target) {
+                return false;
+            }
 
-        @Override
-        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
-            super.onDraw(c, parent, state);
-        }
 
-        @Override
-        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-            super.onDrawOver(c, parent, state);
-        }
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                    float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
 
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            super.getItemOffsets(outRect, view, parent, state);
-        }
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(),
+                        itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                if (listener != null) {
+                    final int position = viewHolder.getAdapterPosition();
+                    listener.onItemSwiped(position);
+                    adapter.removeItem(position);
+                }
+            }
     };
 }
